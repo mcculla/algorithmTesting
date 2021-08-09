d1434e48089f955c8a1b500577cab71f0030c8e5
Austin A. McCullough <amccullough@wustl.edu>
2021-08-09 14:06:06 -0500

    fixed syntax error

--- .vscode/insertionSort.py
+++ .vscode/insertionSort.py
@@ -1,13 +1,13 @@
 def insertionSort(list):
 
-    for sortIndex in range(2, len(list)):
+    for sortIndex in range(1, len(list)):
         keyValue = list[sortIndex]
         compareIndex = sortIndex - 1
 
-        while compareIndex > 0 and list[compareIndex] > keyValue
+        while compareIndex > 0 and list[compareIndex] > keyValue:
             list[compareIndex + 1] = list[compareIndex]
             compareIndex = compareIndex - 1
         
-        list[compareIndex + 1] = key
+        list[compareIndex + 1] = keyValue
 
     return list
