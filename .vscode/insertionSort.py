e736d6bc412946268ef8ff143f5fc10325acc31e
Austin A. McCullough <amccullough@wustl.edu>
2021-08-10 08:58:17 -0500

    Integrated driver code for self-containment

--- .vscode/insertionSort.py
+++ .vscode/insertionSort.py
@@ -1,14 +1,30 @@
-def insertionSort(list):
+def insertionSort(arr):
 
-    for sortIndex in range(1, len(list)):
-        keyValue = list[sortIndex]
+    for sortIndex in range(1, len(arr)):
+        keyValue = arr[sortIndex]
         compareIndex = sortIndex - 1
 
-        while compareIndex >= 0 and list[compareIndex] > keyValue:
-            list[compareIndex + 1] = list[compareIndex]
+        while compareIndex >= 0 and arr[compareIndex] > keyValue:
+            arr[compareIndex + 1] = arr[compareIndex]
             compareIndex = compareIndex - 1
         
-        list[compareIndex + 1] = keyValue
-        print(list)
+        arr[compareIndex + 1] = keyValue
 
-    return list
+
+
+def printArray(arr):
+
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
+
+
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input("Enter numbers separated by a space: ").split()))
+    print("Initial array: ", end="\n")
+    printArray(arr)
+    insertionSort(arr)
+    print("Result: ", end="\n")
+    printArray(arr)
